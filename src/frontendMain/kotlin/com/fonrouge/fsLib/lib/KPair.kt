47e5856cb90669564f9daeaf07b8002cc592292a
Teo Fonrouge <teo@fonrouge.com>
2022-08-20 18:06:32 -0500

    * update

--- src/frontendMain/kotlin/com/fonrouge/fsLib/lib/KPair.kt
+++ src/frontendMain/kotlin/com/fonrouge/fsLib/lib/KPair.kt
@@ -8,9 +8,10 @@ class KPair<T, V>(
     val value: V,
 )
 
-infix fun <T> KProperty1<T, Int?>.with(that: Int?): KPair<T, Int?> = KPair(this, that)
-infix fun <T> KProperty1<T, Boolean?>.with(that: Boolean?): KPair<T, Boolean?> = KPair(this, that)
-infix fun <T> KProperty1<T, String?>.with(that: String?): KPair<T, String?> = KPair(this, that)
-infix fun <T> KProperty1<T, Date?>.with(that: Date?): KPair<T, Date?> = KPair(this, that)
-infix fun <T> KProperty1<T, Double?>.with(that: Double?): KPair<T, Double?> = KPair(this, that)
-infix fun <T> KProperty1<T, Float?>.with(that: Float?): KPair<T, Float?> = KPair(this, that)
+infix fun <T> KProperty1<T, Int?>.with(that: Int?) = KPair(this, that)
+infix fun <T> KProperty1<T, Boolean?>.with(that: Boolean?) = KPair(this, that)
+infix fun <T> KProperty1<T, String?>.with(that: String?) = KPair(this, that)
+infix fun <T> KProperty1<T, Date?>.with(that: Date?) = KPair(this, that)
+infix fun <T> KProperty1<T, Double?>.with(that: Double?) = KPair(this, that)
+infix fun <T> KProperty1<T, Float?>.with(that: Float?) = KPair(this, that)
+infix fun <T> KProperty1<T, Any?>.with(that: Any?) = KPair(this, that)
