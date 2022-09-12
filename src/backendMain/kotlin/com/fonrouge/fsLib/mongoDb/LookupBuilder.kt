934005c1e68e4469227a0472a6cfcd1c4575b3d2
Teo Fonrouge <teo@fonrouge.com>
2022-09-11 21:35:33 -0500

    * update

--- src/backendMain/kotlin/com/fonrouge/fsLib/mongoDb/LookupBuilder.kt
+++ src/backendMain/kotlin/com/fonrouge/fsLib/mongoDb/LookupBuilder.kt
@@ -13,7 +13,7 @@ import kotlin.reflect.KProperty1
 
 class LookupBuilder<T : BaseModel<*>, U : BaseModel<W>, V, W>(
     private val cTableDb: KClass<out CTableDb<U, W>>,
-    private val fieldToField: FieldToField<T, U, V>,
+    private val localToForeign: LocalToForeign<T, U, V>,
     val resultProperty: KProperty1<T, U?>,
     private val matchFilters: List<Bson>? = null
 ) {
@@ -22,7 +22,7 @@ class LookupBuilder<T : BaseModel<*>, U : BaseModel<W>, V, W>(
         val match = mutableListOf(
             expr(
                 eq from listOf(
-                    fieldToField.foreign,
+                    localToForeign.foreign,
                     "$\$letVar1"
                 )
             )
@@ -40,14 +40,14 @@ class LookupBuilder<T : BaseModel<*>, U : BaseModel<W>, V, W>(
 
         pipeline += lookup(
             from = map1[cTableDb]?.mongoColl?.namespace?.collectionName ?: "?",
-            let = listOf(Variable("letVar1", fieldToField.local)),
+            let = listOf(Variable("letVar1", localToForeign.local)),
             resultProperty = resultProperty,
             *pip2.toTypedArray()
         )
         pipeline += resultProperty.unwind(UnwindOptions().preserveNullAndEmptyArrays(true))
     }
 
-    class FieldToField<T, U, V>(
+    class LocalToForeign<T, U, V>(
         val local: KProperty1<T, V>,
         val foreign: KProperty1<U, V>
     )
@@ -55,20 +55,20 @@ class LookupBuilder<T : BaseModel<*>, U : BaseModel<W>, V, W>(
 
 @Suppress("unused")
 infix fun <T : BaseModel<*>, U : BaseModel<*>> KProperty1<T, String?>.localToForeign(that: KProperty1<U, String?>) =
-    LookupBuilder.FieldToField<T, U, String?>(this, that)
+    LookupBuilder.LocalToForeign<T, U, String?>(this, that)
 
 @JvmName("localToForeignTInt?")
 @Suppress("unused")
 infix fun <T : BaseModel<*>, U : BaseModel<*>> KProperty1<T, Int?>.localToForeign(that: KProperty1<U, Int?>) =
-    LookupBuilder.FieldToField<T, U, Int?>(this, that)
+    LookupBuilder.LocalToForeign<T, U, Int?>(this, that)
 
 @JvmName("localToForeignTLong?")
 @Suppress("unused")
 infix fun <T : BaseModel<*>, U : BaseModel<*>> KProperty1<T, Long?>.localToForeign(that: KProperty1<U, Long?>) =
-    LookupBuilder.FieldToField<T, U, Long?>(this, that)
+    LookupBuilder.LocalToForeign<T, U, Long?>(this, that)
 
 @JvmName("localToForeignTLocalDateTime?")
 @Suppress("unused")
 infix fun <T : BaseModel<*>, U : BaseModel<*>> KProperty1<T, LocalDateTime?>.localToForeign(that: KProperty1<U, LocalDateTime?>) =
-    LookupBuilder.FieldToField<T, U, LocalDateTime?>(this, that)
+    LookupBuilder.LocalToForeign<T, U, LocalDateTime?>(this, that)
 
