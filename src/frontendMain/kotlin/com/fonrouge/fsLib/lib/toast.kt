08228d9d4c933d8be0632c92dc624ab53e342b6a
Teo <teo@fonrouge.com>
2023-02-25 15:41:12 -0600

    * update

--- src/frontendMain/kotlin/com/fonrouge/fsLib/lib/toast.kt
+++ src/frontendMain/kotlin/com/fonrouge/fsLib/lib/toast.kt
@@ -1,11 +1,11 @@
 package com.fonrouge.fsLib.lib
 
-import com.fonrouge.fsLib.model.SimpleResponse
+import com.fonrouge.fsLib.model.ISimpleResponse
 import io.kvision.toast.Toast
 import io.kvision.toast.ToastOptions
 
 @Suppress("unused")
-fun SimpleResponse.toast() {
+fun ISimpleResponse.toast() {
     val options = ToastOptions()
     when (isOk) {
         true -> Toast.success(message = msgOk ?: "Ok", options = options)
