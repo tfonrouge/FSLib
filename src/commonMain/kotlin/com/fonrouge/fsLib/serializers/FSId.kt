560fc7747b4cff04e98d872e0799b9ed8e9ea4a5
Teo <teo@fonrouge.com>
2023-03-09 13:45:31 -0600

    * update

--- src/commonMain/kotlin/com/fonrouge/fsLib/serializers/FSId.kt
+++ src/commonMain/kotlin/com/fonrouge/fsLib/serializers/FSId.kt
@@ -3,10 +3,18 @@ package com.fonrouge.fsLib.serializers
 import com.fonrouge.fsLib.model.base.BaseModel
 import com.fonrouge.fsLib.newObjectId
 import kotlinx.serialization.Serializable
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
 import kotlin.reflect.KClass
 
+@OptIn(ExperimentalJsExport::class)
 @Serializable(with = FSIdSerializer::class)
+@JsExport
 class FSId<T : BaseModel<*>>(
     val id: String = newObjectId(),
-    val klass: KClass<T>? = null
-)
+    @Suppress("NON_EXPORTABLE_TYPE") private val klass: KClass<T>? = null
+) {
+    fun name(): String? {
+        return klass?.simpleName
+    }
+}
