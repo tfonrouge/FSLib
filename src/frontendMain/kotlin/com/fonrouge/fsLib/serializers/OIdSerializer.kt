2d8f91e8fe5573b7e34c4a85c8bce5e86e540be6
Teo Fonrouge <teo@fonrouge.com>
2023-03-26 15:41:04 -0600

    * update

--- src/frontendMain/kotlin/com/fonrouge/fsLib/serializers/OIdSerializer.kt
+++ src/frontendMain/kotlin/com/fonrouge/fsLib/serializers/OIdSerializer.kt
@@ -6,6 +6,7 @@ import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlin.js.Date
 
 actual object OIdSerializer : KSerializer<OId<Any>> {
     override fun deserialize(decoder: Decoder): OId<Any> {
@@ -18,4 +19,14 @@ actual object OIdSerializer : KSerializer<OId<Any>> {
     override fun serialize(encoder: Encoder, value: OId<Any>) {
         encoder.encodeString(value.id)
     }
+}
+
+/**
+ * Get a [Date] from [OId] value
+ */
+@Suppress("unused")
+val OId<out Any>?.date : Date? get() {
+    @Suppress("UNUSED_VARIABLE") val hex = this?.id?.substring(0..7)
+    val i = js("parseInt(hex, 16) * 1000") as? Int
+    return i?.let { Date(i) }
 }
