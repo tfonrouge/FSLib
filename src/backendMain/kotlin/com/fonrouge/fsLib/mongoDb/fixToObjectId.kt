a221bd14bd838849972d948da970e94b79bb2b74
Teo <teo@fonrouge.com>
2023-03-26 09:39:05 -0600

    * renamed annotation property collection -> name

---
+++ src/backendMain/kotlin/com/fonrouge/fsLib/mongoDb/fixToObjectId.kt
@@ -0,0 +1,63 @@
+package com.fonrouge.fsLib.mongoDb
+
+import com.mongodb.MongoNamespace
+import com.mongodb.client.model.InsertOneModel
+import com.mongodb.client.model.WriteModel
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.plugins.httpsredirect.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+import io.ktor.util.*
+import kotlinx.coroutines.reactive.asFlow
+import org.bson.BsonObjectId
+import org.bson.Document
+import org.bson.types.ObjectId
+import org.litote.kmongo.*
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.coroutine.toList
+import java.util.*
+
+@Suppress("unused")
+suspend fun fixToObjectId(cTableDb: CTableDb<*, *>): Boolean {
+    val collName = cTableDb.collectionName
+    val newCollName = "new$collName"
+    if (collName !in mongoDatabase.listCollectionNames().toList()) return false
+    if (newCollName in mongoDatabase.listCollectionNames().toList()) return false
+    val coll = mongoDatabase.getCollection(collName)
+    val list = mutableListOf<WriteModel<Document>>()
+    coll.find().asFlow().collect { document ->
+        when (val id = document["_id"]) {
+            is String -> {
+                document["_id"] = when (id.length) {
+                    24 -> {
+                        ObjectId(id)
+                    }
+
+                    else -> {
+                        ObjectId(Base64.getUrlDecoder().decode(document.getString("_id")))
+                    }
+                }.let { objectId -> BsonObjectId(objectId) }
+                list.add(InsertOneModel(document))
+            }
+        }
+    }
+    if (list.isNotEmpty()) {
+        val newColl = mongoDatabase.getCollection(newCollName)
+        val r = newColl.coroutine.bulkWrite(list)
+        println("*** Conversion of collection '$collName' inserted documents = ${r.insertedCount}")
+        mongoDatabase.coroutine.dropCollection(collName)
+        mongoDatabase.getCollection(newCollName)
+            .renameCollection(MongoNamespace(mongoDatabase.name, collName))
+            .asFlow()
+            .collect {
+                println(it)
+            }
+    }
+    return true
+}
