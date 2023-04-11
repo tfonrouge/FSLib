5046c877b108ec2a14e3afc38f8fcb8d92663760
Teo <teo@fonrouge.com>
2023-04-10 20:58:16 -0600

    * update

---
+++ src/backendMain/kotlin/com/fonrouge/fsLib/ktor/decodeFromString.kt
@@ -0,0 +1,17 @@
+package com.fonrouge.fsLib.ktor
+
+import io.ktor.http.*
+import io.ktor.server.util.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+
+/**
+ * Helper to deserialize query parameters
+ *
+ * @param s string to deserialize
+ * @return type [T]
+ */
+@Suppress("unused")
+inline fun <reified T> Parameters.decodeFromString(s: String): T? {
+    return Json.decodeFromString(getOrFail(s))
+}
