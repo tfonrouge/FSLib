6a3af72e40f07305c321ef1332bd6cb29bd170ba
Teo <teo@fonrouge.com>
2023-04-17 17:48:51 -0600

    ! stupid error

--- src/commonMain/kotlin/com/fonrouge/fsLib/model/state/ContexState.kt
+++ src/commonMain/kotlin/com/fonrouge/fsLib/model/state/ContexState.kt
@@ -6,8 +6,8 @@ import kotlinx.serialization.json.Json
 
 @Serializable
 abstract class ContexState : IContextState {
-    inline fun <reified V> contextIdValue(): V {
-        return Json.decodeFromString(contextId ?: "")
+    inline fun <reified V> contextIdValue(): V? {
+        return contextId?.let { Json.decodeFromString(it) }
     }
 
     inline fun <reified T> stateValue(): T? {
