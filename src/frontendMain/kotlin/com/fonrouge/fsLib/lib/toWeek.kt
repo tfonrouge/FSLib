1720558f3c99e307309e1ca328bdf9c9ed5f1516
teo <teo@fonrouge.com>
2023-06-07 17:15:01 -0600

    * update

--- src/frontendMain/kotlin/com/fonrouge/fsLib/lib/toWeek.kt
+++ src/frontendMain/kotlin/com/fonrouge/fsLib/lib/toWeek.kt
@@ -24,3 +24,27 @@ val Date.toWeek: String
         val l2 = convert(localDateJsJ.plusDays(6)).toDate().toLocaleDateString("es-MX", opt)
         return "${localDateJsJ.isoWeekOfWeekyear()} ($l1 - $l2)"
     }
+
+@Suppress("unused")
+val Date.toISOWeek: String
+    get() {
+        val localDateJsJ = LocalDate.of(
+            year = getFullYear(),
+            month = getMonth() + 1,
+            dayOfMonth = getDate()
+        )
+        return "${localDateJsJ.year()}-W${localDateJsJ.isoWeekOfWeekyear()}"
+    }
+
+@Suppress("unused")
+/* TODO: use a more precise/std method */
+fun isoWeekToDate(isoWeek: String): Date {
+    val year = isoWeek.substring(0..3).toInt()
+    val localDateJsJ = LocalDate.of(
+        year = year,
+        month = 1,
+        dayOfMonth = 4
+    )
+    val week = isoWeek.substring(6..7).toInt() - 1
+    return convert(localDateJsJ.plusWeeks(week)).toDate()
+}
