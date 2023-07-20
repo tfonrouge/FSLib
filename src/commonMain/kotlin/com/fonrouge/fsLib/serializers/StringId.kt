d2d1a5cc6d8f5ad70bfd8fbc1dbb9459b88e2724
teo <teo@fonrouge.com>
2023-07-20 10:58:58 -0600

    * version to 1.7.2

--- src/commonMain/kotlin/com/fonrouge/fsLib/serializers/StringId.kt
+++ src/commonMain/kotlin/com/fonrouge/fsLib/serializers/StringId.kt
@@ -1,11 +1,10 @@
 package com.fonrouge.fsLib.serializers
 
-import com.fonrouge.fsLib.objectIdHexString
 import kotlinx.serialization.Serializable
 
 @Serializable(with = StringIdSerializer::class)
 data class StringId<@Suppress("unused") T>(
-    val id: String = objectIdHexString(),
+    val id: String = "",
 ) {
     override fun toString(): String {
         return id
