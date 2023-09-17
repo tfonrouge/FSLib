ce1e0961450d8f7102e9703f973d56356f201e60
Teo Fonrouge <teo@fonrouge.com>
2023-09-17 16:19:31 -0600

    * update

--- src/commonMain/kotlin/com/fonrouge/fsLib/serializers/FSOffsetDateTimeSerializer.kt
+++ src/commonMain/kotlin/com/fonrouge/fsLib/serializers/FSOffsetDateTimeSerializer.kt
@@ -3,6 +3,6 @@ package com.fonrouge.fsLib.serializers
 import io.kvision.types.OffsetDateTime
 import kotlinx.serialization.KSerializer
 
-const val KV_DEFAULT_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss"
+const val KV_DEFAULT_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS"
 
 expect object FSOffsetDateTimeSerializer : KSerializer<OffsetDateTime>
