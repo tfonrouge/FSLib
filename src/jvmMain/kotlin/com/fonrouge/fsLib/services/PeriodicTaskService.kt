583886dd2fe8475d3febaabd2672a4dcc1dceed8
Teo Fonrouge <teo@fonrouge.com>
2023-09-30 13:05:55 -0600

    * update

--- src/jvmMain/kotlin/com/fonrouge/fsLib/services/PeriodicTaskService.kt
+++ src/jvmMain/kotlin/com/fonrouge/fsLib/services/PeriodicTaskService.kt
@@ -1,91 +1,120 @@
 package com.fonrouge.fsLib.services
 
+import com.fonrouge.fsLib.services.Task.TimeUnit
 import io.ktor.server.application.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import org.koin.java.KoinJavaComponent.inject
 import org.koin.ktor.plugin.koin
 import java.time.OffsetDateTime
+import kotlin.reflect.KCallable
 import kotlin.reflect.KClass
 import kotlin.reflect.full.callSuspend
+import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.hasAnnotation
 
 interface IPeriodicTaskService {
     var workingTaskMap: MutableSet<String>
 }
 
-enum class TimeUnit {
-    Second,
-    Minute,
-    Hour,
-    Day,
-    Month,
-    Year,
-}
-
 /**
  * Starts a periodic task runner for each function marked with @Task annotation in the [klass] parameter class
  *
  * @param klass Kotlin class which contains functions (marked with the @Task annotation) to be executed
- * @param timeUnit Time unit for periodicity [TimeUnit]
  */
 @Suppress("unused")
 fun Application.startPeriodicTask(
     klass: KClass<out IPeriodicTaskService>,
-    timeUnit: TimeUnit = TimeUnit.Minute,
     debug: Boolean = false,
 ) {
     koin {
         val periodicTask by inject<IPeriodicTaskService>(klass.java)
+        val pairPeriodFunc: List<Pair<TimeUnit, KCallable<*>>> = klass.members.mapNotNull { kFunction ->
+            kFunction.findAnnotation<Task>()?.let { task: Task ->
+                task.timeUnit to kFunction
+            }
+        }
+        var previousTime = OffsetDateTime.MIN
         flow {
             if (debug) {
-                println("Installing ${klass.simpleName} with [$timeUnit] periodicity ...")
+                println("Installing Periodic Task Service ${klass.simpleName} ...")
             }
             while (true) {
-                emit(Unit)
-                delay(1000)
+                emit(OffsetDateTime.now())
+                delay(200)
             }
-        }.map { OffsetDateTime.now() }
-            .distinctUntilChanged { old: OffsetDateTime, new: OffsetDateTime ->
-                when (timeUnit) {
-                    TimeUnit.Second -> old.second == new.second
-                    TimeUnit.Minute -> old.minute == new.minute
-                    TimeUnit.Hour -> old.hour == new.hour
-                    TimeUnit.Day -> old.dayOfYear == new.dayOfYear
-                    TimeUnit.Month -> old.month == new.month
-                    TimeUnit.Year -> old.year == new.year
+        }.onEach { now: OffsetDateTime ->
+            val funcsToCall = mutableListOf<KCallable<*>>()
+            if (previousTime.second != now.second) {
+                TimeUnit.entries.forEach { timeUnit ->
+                    val funcsForTimeUnit: List<KCallable<*>> =
+                        pairPeriodFunc.filter { it.first == timeUnit }.map { it.second }
+                    when (timeUnit) {
+                        TimeUnit.Second -> funcsToCall.addAll(funcsForTimeUnit)
+                        TimeUnit.Minute -> if (previousTime.minute != now.minute) funcsToCall.addAll(
+                            funcsForTimeUnit
+                        )
+
+                        TimeUnit.Hour -> if (previousTime.hour != now.hour) funcsToCall.addAll(funcsForTimeUnit)
+                        TimeUnit.Day -> if (previousTime.dayOfYear != now.dayOfYear) funcsToCall.addAll(
+                            funcsForTimeUnit
+                        )
+
+                        TimeUnit.Month -> if (previousTime.monthValue != now.monthValue) funcsToCall.addAll(
+                            funcsForTimeUnit
+                        )
+
+                        TimeUnit.Year -> if (previousTime.year != now.year) funcsToCall.addAll(funcsForTimeUnit)
+                    }
                 }
-            }.onEach {
-                klass.members.forEach { kCallable ->
-                    if (kCallable.hasAnnotation<Task>()) {
-                        if (!periodicTask.workingTaskMap.contains(kCallable.name)) {
-                            periodicTask.workingTaskMap.add(kCallable.name)
+                previousTime = now
+            }
+            funcsToCall.forEach { kCallable ->
+                if (kCallable.hasAnnotation<Task>()) {
+                    if (!periodicTask.workingTaskMap.contains(kCallable.name)) {
+                        periodicTask.workingTaskMap.add(kCallable.name)
+                        if (debug) {
+                            println("* Periodic Task Service: Starting ${klass.simpleName}::${kCallable.name}")
+                        }
+                        this@startPeriodicTask.launch {
+                            try {
+                                kCallable.callSuspend(periodicTask)
+                            } catch (e: Exception) {
+                                val msgErr = e.message ?: e.cause?.message
+                                System.err.println("* Periodic Task Service: Error ${klass.simpleName}::${kCallable.name} = $msgErr")
+                                e.printStackTrace()
+                            }
                             if (debug) {
-                                println("* Periodic Task Service: Starting ${klass.simpleName}::${kCallable.name}")
+                                println("* Periodic Task Service: Finalizing ${klass.simpleName}::${kCallable.name}")
                             }
-                            this@startPeriodicTask.launch {
-                                try {
-                                    kCallable.callSuspend(periodicTask)
-                                } catch (e: Exception) {
-                                    val msgErr = e.message ?: e.cause?.message
-                                    System.err.println("* Periodic Task Service: Error ${klass.simpleName}::${kCallable.name} = $msgErr")
-                                    e.printStackTrace()
-                                }
-                                if (debug) {
-                                    println("* Periodic Task Service: Finalizing ${klass.simpleName}::${kCallable.name}")
-                                }
-                                periodicTask.workingTaskMap.remove(kCallable.name)
-                            }
+                            periodicTask.workingTaskMap.remove(kCallable.name)
                         }
                     }
                 }
-            }.launchIn(CoroutineScope(Dispatchers.Default))
+            }
+        }.launchIn(CoroutineScope(Dispatchers.Default))
     }
 }
 
+/**
+ * @param timeUnit Time unit for periodicity [TimeUnit]
+ */
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class Task
+annotation class Task(
+    val timeUnit: TimeUnit = TimeUnit.Minute,
+) {
+    enum class TimeUnit {
+        Second,
+        Minute,
+        Hour,
+        Day,
+        Month,
+        Year,
+    }
+}
