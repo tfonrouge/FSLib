02c81502ed37f9edf72a9db112c8edb8acc988ae
Teo Fonrouge <teo@fonrouge.com>
2023-12-30 00:41:39 -0600

    * update

--- src/commonMain/kotlin/com/fonrouge/fsLib/model/base/AppRole.kt
+++ src/commonMain/kotlin/com/fonrouge/fsLib/model/base/AppRole.kt
@@ -2,6 +2,8 @@ package com.fonrouge.fsLib.model.base
 
 import com.fonrouge.fsLib.annotations.Collection
 import com.fonrouge.fsLib.serializers.OId
+import com.fonrouge.fsLib.serializers.XEnum
+import com.fonrouge.fsLib.serializers.XEnumSerializer
 import kotlinx.serialization.Serializable
 
 @Suppress("unused")
@@ -11,14 +13,21 @@ class AppRole(
     override val _id: OId<AppRole> = OId(),
     val classOwner: String,
     val funcName: String,
-    val roleType: RoleType,
+    val roleType: RoleType = RoleType.Simple,
     val description: String?,
     val detail: String?,
     val defaultPermission: PermissionType = PermissionType.Deny
 ) : BaseDoc<OId<AppRole>> {
     @Serializable
-    enum class RoleType(val description: String) {
-        S("Simple"),
-        DA("Data Action"),
+    enum class RoleType(
+        override val encoded: String,
+        override val label: String = ""
+    ) : XEnum {
+        Simple("S"),
+        DataAction("DA"),
     }
+}
+
+object RoleTypeSerializer : XEnumSerializer<AppRole.RoleType>() {
+    override fun enumEntries(): List<AppRole.RoleType> = AppRole.RoleType.entries
 }
--- src/commonMain/kotlin/com/fonrouge/fsLib/model/base/PermissionType.kt
+++ src/commonMain/kotlin/com/fonrouge/fsLib/model/base/PermissionType.kt
@@ -1,30 +1,19 @@
 package com.fonrouge.fsLib.model.base
 
-import kotlinx.serialization.KSerializer
+import com.fonrouge.fsLib.serializers.XEnum
+import com.fonrouge.fsLib.serializers.XEnumSerializer
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 
 @Serializable(with = PermissionTypeSerializer::class)
-enum class PermissionType(val id: String) {
+enum class PermissionType(
+    override val encoded: String,
+    override val label: String = ""
+) : XEnum {
     Allow("1"),
     Deny("0"),
-    Default("D"),
+    Default("D")
 }
 
-object PermissionTypeSerializer : KSerializer<PermissionType> {
-    override val descriptor: SerialDescriptor
-        get() = PrimitiveSerialDescriptor("PermissionType", PrimitiveKind.STRING)
-
-    override fun serialize(encoder: Encoder, value: PermissionType) {
-        encoder.encodeString(value = value.id)
-    }
-
-    override fun deserialize(decoder: Decoder): PermissionType {
-        val code = decoder.decodeString()
-        return PermissionType.values().first { it.id == code }
-    }
+object PermissionTypeSerializer : XEnumSerializer<PermissionType>() {
+    override fun enumEntries(): List<PermissionType> = PermissionType.entries
 }
