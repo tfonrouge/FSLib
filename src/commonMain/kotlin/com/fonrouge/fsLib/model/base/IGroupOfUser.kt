77c9dcc954b298052a2b4540dfe319dd4b6b4b1d
Teo Fonrouge <155397527+olanmex@users.noreply.github.com>
2024-01-03 19:12:51 -0600

    * minor code cleanup

--- src/commonMain/kotlin/com/fonrouge/fsLib/model/base/IGroupOfUser.kt
+++ src/commonMain/kotlin/com/fonrouge/fsLib/model/base/IGroupOfUser.kt
@@ -2,7 +2,7 @@ package com.fonrouge.fsLib.model.base
 
 import com.fonrouge.fsLib.serializers.OId
 
-interface IGroupOfUser<T: Any> : BaseDoc<OId<T>> {
+interface IGroupOfUser<T : Any> : BaseDoc<OId<T>> {
     override val _id: OId<T>
     val description: String
 }
