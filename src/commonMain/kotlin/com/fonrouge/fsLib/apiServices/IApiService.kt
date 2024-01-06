e91e9f5e288796e1ca1beccf2030f26005348de2
Teo Fonrouge <155397527+olanmex@users.noreply.github.com>
2024-01-06 06:23:26 -0600

    * update

--- src/commonMain/kotlin/com/fonrouge/fsLib/apiServices/IApiService.kt
+++ src/commonMain/kotlin/com/fonrouge/fsLib/apiServices/IApiService.kt
@@ -1,4 +1,3 @@
 package com.fonrouge.fsLib.apiServices
 
-@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 expect interface IApiService
--- src/commonMain/kotlin/com/fonrouge/fsLib/config/ICommonView.kt
+++ src/commonMain/kotlin/com/fonrouge/fsLib/config/ICommonView.kt
@@ -2,6 +2,7 @@ package com.fonrouge.fsLib.config
 
 import com.fonrouge.fsLib.model.apiData.IApiFilter
 
+// TODO: integrate with IApiService
 abstract class ICommonView<FILT : IApiFilter>(
     val label: String,
 ) {
