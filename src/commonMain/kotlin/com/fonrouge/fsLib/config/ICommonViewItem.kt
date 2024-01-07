c64180f12a2818a763f2273cff85f0162c235d1d
Teo Fonrouge <155397527+olanmex@users.noreply.github.com>
2024-01-07 15:58:09 -0600

    * update

--- src/commonMain/kotlin/com/fonrouge/fsLib/config/ICommonViewItem.kt
+++ src/commonMain/kotlin/com/fonrouge/fsLib/config/ICommonViewItem.kt
@@ -5,7 +5,6 @@ import com.fonrouge.fsLib.model.apiData.IApiFilter
 import com.fonrouge.fsLib.model.base.BaseDoc
 import com.fonrouge.fsLib.model.state.ItemState
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.serializer
 
 abstract class ICommonViewItem<T : BaseDoc<ID>, ID : Any, FILT : IApiFilter>(
     val labelIdFunc: ((T?) -> String)? = { it?._id?.toString() ?: "<no-item>" },
@@ -17,10 +16,10 @@ abstract class ICommonViewItem<T : BaseDoc<ID>, ID : Any, FILT : IApiFilter>(
     label = label,
     apiFilterSerializer = apiFilterSerializer
 ) {
-    var apiItem: ApiItem<T, ID, FILT>? = null
+    //    var apiItem: ApiItem<T, ID, FILT>? = null
     var itemState: ItemState<T>? = null
-    var onQueryFail: (ICommonViewItem<T, ID, FILT>.() -> Unit)? = null
-    var onQuerySuccess: (ICommonViewItem<T, ID, FILT>.() -> Unit)? = null
-    var onActionFail: (ICommonViewItem<T, ID, FILT>.() -> Unit)? = null
-    var onActionSuccess: (ICommonViewItem<T, ID, FILT>.() -> Unit)? = null
+    var onQueryFail: (ICommonViewItem<T, ID, FILT>.(ApiItem<T, ID, FILT>?) -> Unit)? = null
+    var onQuerySuccess: (ICommonViewItem<T, ID, FILT>.(ApiItem<T, ID, FILT>?) -> Unit)? = null
+    var onActionFail: (ICommonViewItem<T, ID, FILT>.(ApiItem<T, ID, FILT>?) -> Unit)? = null
+    var onActionSuccess: (ICommonViewItem<T, ID, FILT>.(ApiItem<T, ID, FILT>?) -> Unit)? = null
 }
