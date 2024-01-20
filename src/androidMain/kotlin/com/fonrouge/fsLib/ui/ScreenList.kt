f0e340fc1bbfc60a860acd8c28877af322ec87b3
Teo Fonrouge <155397527+olanmex@users.noreply.github.com>
2024-01-19 21:37:39 -0600

    * update

---
+++ src/androidMain/kotlin/com/fonrouge/fsLib/ui/ScreenList.kt
@@ -0,0 +1,85 @@
+package com.fonrouge.fsLib.ui
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import com.fonrouge.fsLib.model.apiData.IApiFilter
+import com.fonrouge.fsLib.model.base.BaseDoc
+import com.fonrouge.fsLib.viewModel.ViewModelPagingData
+
+@Suppress("unused")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+inline fun <reified VML : ViewModelPagingData<T, FILT>, T : BaseDoc<*>, FILT : IApiFilter> ScreenList(
+    navHostController: NavHostController = NavHostController(LocalContext.current),
+    viewModel: VML = viewModel(),
+    noinline topBarTitle: @Composable () -> Unit = {},
+    noinline topBarNavigationIcon: @Composable () -> Unit = {
+        IconButton(onClick = { navHostController.popBackStack() }) {
+            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+        }
+    },
+    noinline topBarActions: @Composable RowScope.() -> Unit = {},
+    noinline bottomBar: @Composable () -> Unit = {},
+    noinline modalNavigationDrawerContent: @Composable ColumnScope.(DrawerState) -> Unit = {},
+    noinline bodyListContent: @Composable (T?) -> Unit
+) {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState(viewModel)) },
+        topBar = {
+            TopAppBar(
+                title = topBarTitle,
+                navigationIcon = topBarNavigationIcon,
+                actions = topBarActions
+            )
+        },
+        bottomBar = bottomBar
+    ) { paddingValues ->
+        ModalNavigationDrawer(
+            modifier = Modifier.padding(paddingValues),
+            drawerState = drawerState,
+            drawerContent = {
+                ScreenFilter1(
+                    drawerState = drawerState,
+                    content = modalNavigationDrawerContent
+                )
+            }
+        ) {
+            BodyList(
+                viewModel = viewModel,
+                pullRefreshState = pullRefreshState(viewModel = viewModel),
+                content = bodyListContent
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ScreenFilter1(
+    drawerState: DrawerState = rememberDrawerState(initialValue = DrawerValue.Closed),
+    content: @Composable ColumnScope.(DrawerState) -> Unit = {}
+) {
+    ModalDrawerSheet(
+        modifier = Modifier
+            .fillMaxSize(0.8f)
+            .padding(top = 10.dp),
+    ) {
+        Spacer(modifier = Modifier.size(20.dp))
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            content = { content(drawerState) }
+        )
+    }
+}
