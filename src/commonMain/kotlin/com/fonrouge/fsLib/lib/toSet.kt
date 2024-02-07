418cbca2d79539a184655488ab10372b0e30c851
Teo Fonrouge <155397527+olanmex@users.noreply.github.com>
2024-02-07 11:06:14 -0600

    * update

---
+++ src/commonMain/kotlin/com/fonrouge/fsLib/lib/toSet.kt
@@ -0,0 +1,19 @@
+package com.fonrouge.fsLib.lib
+
+import com.fonrouge.fsLib.model.base.BaseDoc
+import com.fonrouge.fsLib.serializers.IntId
+import com.fonrouge.fsLib.serializers.LongId
+import com.fonrouge.fsLib.serializers.OId
+import com.fonrouge.fsLib.serializers.StringId
+
+@Suppress("unused")
+fun <T : BaseDoc<*>> String?.toStringIdSet(): Set<StringId<T>>? = this?.split(',')?.map { StringId<T>(it) }?.toSet()
+
+@Suppress("unused")
+fun <T : BaseDoc<*>> String?.toIntIdSet(): Set<IntId<T>>? = this?.split(',')?.map { IntId<T>(it.toInt()) }?.toSet()
+
+@Suppress("unused")
+fun <T : BaseDoc<*>> String?.toLongIdSet(): Set<LongId<T>>? = this?.split(',')?.map { LongId<T>(it.toLong()) }?.toSet()
+
+@Suppress("unused")
+fun <T : BaseDoc<*>> String?.toOIdSet(): Set<OId<T>>? = this?.split(',')?.map { OId<T>(it) }?.toSet()
