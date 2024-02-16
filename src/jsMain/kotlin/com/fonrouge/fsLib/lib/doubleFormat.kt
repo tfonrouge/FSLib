cebad04cedc8051c57d11f400a940e09a8a9aa6c
Teo Fonrouge <155397527+olanmex@users.noreply.github.com>
2024-02-15 20:37:06 -0600

    * update

--- src/jsMain/kotlin/com/fonrouge/fsLib/lib/doubleFormat.kt
+++ src/jsMain/kotlin/com/fonrouge/fsLib/lib/doubleFormat.kt
@@ -3,7 +3,7 @@ package com.fonrouge.fsLib.lib
 import io.kvision.utils.numberFormat
 
 @Suppress("unused")
-fun Double?.format(decimals: Int): String {
+fun Number?.format(decimals: Int): String {
     return this?.let {
         numberFormat {
             minimumFractionDigits = decimals
