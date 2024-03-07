8740065ce3e5a53388c183726ab9fcf14e504c2e
Teo Fonrouge <155397527+olanmex@users.noreply.github.com>
2024-03-07 09:55:56 -0600

    * update

--- src/commonMain/kotlin/com/fonrouge/fsLib/lib/toSet.kt
+++ src/commonMain/kotlin/com/fonrouge/fsLib/lib/toSet.kt
@@ -17,3 +17,58 @@ fun <T : BaseDoc<*>> String.toLongIdSet(): Set<LongId<T>> = this.split(',').map 
 
 @Suppress("unused")
 fun <T : BaseDoc<*>> String.toOIdSet(): Set<OId<T>> = this.split(',').map { OId<T>(it) }.toSet()
+
+/**
+ * Returns a set containing the current `StringId` instance, or null if the instance is null.
+ *
+ * If the current `StringId` instance is not null, it will be wrapped in a set and returned. Otherwise, if the instance is null, null will be returned.
+ *
+ * @param T the type of the `BaseDoc` elements.
+ * @return a Set of `StringId` elements, or null if the current instance is null.
+ */
+@Suppress("unused")
+fun <T : BaseDoc<*>> StringId<T>?.setOf(): Set<StringId<T>>? {
+    return this?.let { setOf(it) }
+}
+
+/**
+ * Returns a set containing the specified [IntId] element if it is not null,
+ * or an empty set if the element is null.
+ *
+ * @return a set containing the specified [IntId] element, or an empty set
+ *         if the element is null
+ */
+@Suppress("unused")
+fun <T : BaseDoc<*>> IntId<T>?.setOf(): Set<IntId<T>>? {
+    return this?.let { setOf(it) }
+}
+
+/**
+ * Returns a set containing the receiver [LongId] object.
+ *
+ * The [setOf] method is an extension function that can be called on nullable [LongId] objects.
+ * It creates a set containing the receiver object if it is not null, otherwise it returns null.
+ * The type parameter [T] must extend [BaseDoc], ensuring that the [LongId] object is compatible with base document types.
+ *
+ * @return a set containing the receiver [LongId] object, or null if the receiver object is null.
+ */
+@Suppress("unused")
+fun <T : BaseDoc<*>> LongId<T>?.setOf(): Set<LongId<T>>? {
+    return this?.let { setOf(it) }
+}
+
+/**
+ * Creates a set containing the receiver [OId] object.
+ *
+ * The [setOf] method is an extension function that can be called on nullable [OId] objects.
+ * It creates a set containing the receiver object if it is not null, otherwise it returns null.
+ * The type parameter [T] must extend [BaseDoc], ensuring that the [OId] object is compatible with base document types.
+ *
+ * @return a set containing the receiver [OId] object, or null if the receiver object is null.
+ */
+@Suppress("unused")
+fun <T : BaseDoc<*>> OId<T>?.setOf(): Set<OId<T>>? {
+    return this?.let { setOf(it) }
+}
+
+
