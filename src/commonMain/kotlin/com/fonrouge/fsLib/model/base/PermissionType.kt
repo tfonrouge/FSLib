5e8ace55bce367f0af521d9ead9eca48c95cb487
Teo Fonrouge <155397527+olanmex@users.noreply.github.com>
2024-04-26 13:28:12 -0600

    * update

--- src/commonMain/kotlin/com/fonrouge/fsLib/model/base/PermissionType.kt
+++ src/commonMain/kotlin/com/fonrouge/fsLib/model/base/PermissionType.kt
@@ -1,12 +1,11 @@
 package com.fonrouge.fsLib.model.base
 
+import com.fonrouge.fsLib.enums.XEnum
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class PermissionType(
-    val label: String = ""
-) {
+enum class PermissionType(override val encoded: String) : XEnum {
     @SerialName("1")
     Allow("1"),
 
