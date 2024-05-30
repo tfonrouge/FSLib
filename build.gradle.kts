35e8fd0d72c3326d797424693f82f2e9104e51c9
Teo Fonrouge <155397527+olanmex@users.noreply.github.com>
2024-05-30 13:40:13 -0600

    * update

--- build.gradle.kts
+++ build.gradle.kts
@@ -146,7 +146,7 @@ android {
     }
     dependencies {
         implementation("androidx.core:core-ktx:1.13.1")
-        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
         implementation("androidx.activity:activity-compose:1.9.0")
         implementation(platform("androidx.compose:compose-bom:2024.05.00"))
         implementation("androidx.compose.ui:ui")
@@ -159,8 +159,8 @@ android {
         implementation("androidx.paging:paging-compose:3.3.0")
         /* scanner service provided by Google Play */
         implementation("com.google.android.gms:play-services-code-scanner:16.1.0")
-        implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
-        api("androidx.lifecycle:lifecycle-runtime-compose:2.8.0")
+        implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
+        api("androidx.lifecycle:lifecycle-runtime-compose:2.8.1")
 
         implementation("androidx.camera:camera-camera2:1.4.0-beta01")
         implementation("androidx.camera:camera-lifecycle:1.4.0-beta01")
@@ -190,7 +190,7 @@ android {
         androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
         androidTestImplementation("androidx.compose.ui:ui-test-junit4")
         debugImplementation("androidx.compose.ui:ui-tooling")
-        debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.0-beta01")
+        debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.0-beta02")
     }
     /*
         buildTypes {
@@ -206,6 +206,6 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11"
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
 }
