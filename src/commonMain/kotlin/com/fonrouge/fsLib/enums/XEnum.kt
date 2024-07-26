149e2d60ec548a022034ea23f6edcc98f5c21873
Teo Fonrouge <teo@fonrouge.com>
2024-07-26 17:59:00 -0600

    + helper functions

--- src/commonMain/kotlin/com/fonrouge/fsLib/enums/XEnum.kt
+++ src/commonMain/kotlin/com/fonrouge/fsLib/enums/XEnum.kt
@@ -1,5 +1,24 @@
 package com.fonrouge.fsLib.enums
 
+import kotlin.enums.EnumEntries
+
 interface XEnum {
     val encoded: String
+}
+
+@Suppress("unused")
+fun EnumEntries<*>.getEncoded(xEnum: XEnum?): String? {
+    val a = firstOrNull {
+        if (it is XEnum) {
+            it.encoded == xEnum?.encoded
+        } else false
+    }
+    return if (a is XEnum) a.encoded else null
+}
+
+@Suppress("unused")
+fun EnumEntries<*>.encodedList(): List<Pair<String, String>> {
+    return map {
+        if (it is XEnum) it.encoded to it.name else it.name to it.name
+    }
 }
