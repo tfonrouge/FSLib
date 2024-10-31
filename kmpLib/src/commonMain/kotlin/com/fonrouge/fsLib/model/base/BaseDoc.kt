3cd2b05c00335402cb5e7d2d0af729c0a900a4c5
Teo Fonrouge <teo@fonrouge.com>
2024-10-30 21:51:31 -0600

    Refactor type parameter in BaseDoc interface
    Changed the type parameter from T to ID to improve code clarity and consistency. This refactor ensures the interface better reflects its purpose by explicitly indicating that the type parameter represents an identifier.

--- kmpLib/src/commonMain/kotlin/com/fonrouge/fsLib/model/base/BaseDoc.kt
+++ kmpLib/src/commonMain/kotlin/com/fonrouge/fsLib/model/base/BaseDoc.kt
@@ -3,10 +3,10 @@ package com.fonrouge.fsLib.model.base
 /**
  * The base class for the items referenced in the frontend-backend transactions
  *
- * If a [Enum] class is used as [_id] [T] type, remember to set the idKClass property on the ConfigViewContainer definition
+ * If a [Enum] class is used as [_id] [ID] type, remember to set the idKClass property on the ConfigViewContainer definition
  * to correctly serialize/deserialize the [_id] when referencing it in the transactions.
  */
-interface BaseDoc<T : Any> {
+interface BaseDoc<ID : Any> {
     @Suppress("PropertyName")
-    val _id: T
+    val _id: ID
 }
