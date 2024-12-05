1fa8cdcfdb5438afb117b1a72c04a4185a440c27
Teo Fonrouge <teo@fonrouge.com>
2024-12-04 22:28:03 -0600

    Add delay parameter to waitForLockValue function
    Introduce a delay between lock status checks to avoid tight loops and improve efficiency. The delay defaults to 100 milliseconds and can be customized by the caller. Also, enhance the function documentation for clarity.

--- kmpLib/src/commonMain/kotlin/com/fonrouge/fsLib/coroutines/waitForLockValue.kt
+++ kmpLib/src/commonMain/kotlin/com/fonrouge/fsLib/coroutines/waitForLockValue.kt
@@ -1,12 +1,23 @@
 package com.fonrouge.fsLib.coroutines
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 import kotlin.reflect.KMutableProperty0
 
+/**
+ * Suspends execution until a mutable boolean property is no longer locked or until the attempt count is exhausted.
+ *
+ * @param kProperty A KMutableProperty0 representing the lock status as a Boolean. It is expected to be `true` if locked and `false` otherwise.
+ * @param attempts The maximum number of attempts to check the lock status before giving up. Defaults to 10.
+ * @param delay The duration in milliseconds to wait between each attempt. Defaults to 100.
+ *
+ * @return A Boolean indicating the final lock status. Returns `true` if the lock was released, otherwise `false`.
+ */
 @Suppress("unused")
 suspend fun waitForLockValue(
     kProperty: KMutableProperty0<Boolean>,
     attempts: Int = 10,
+    delay: Int = 100,
 ): Boolean {
     flow<Boolean> {
         var counter = attempts
@@ -14,6 +25,7 @@ suspend fun waitForLockValue(
             --counter
             if (attempts > 0) {
                 println("waiting for lock release ... $counter attempts left")
+                delay(delay.toLong())
             }
         }
         if (counter == 0) {
