53aa504f67017d5a0381d8558f586515b0588f08
Teo Fonrouge <teo@fonrouge.com>
2024-12-17 02:21:37 -0600

    Add detailed documentation to ConfigData and ConfigViewContainer
    Expanded KDoc comments for `ConfigData` and `ConfigViewContainer` to provide clearer descriptions of their purpose, parameters, and usage. This enhances readability and helps developers better understand the intent and implementation of these classes.

--- kmpLib/src/jsMain/kotlin/com/fonrouge/fsLib/config/ConfigViewContainer.kt
+++ kmpLib/src/jsMain/kotlin/com/fonrouge/fsLib/config/ConfigViewContainer.kt
@@ -6,15 +6,21 @@ import com.fonrouge.fsLib.view.ViewDataContainer
 import kotlin.reflect.KClass
 
 /**
- * A container class for configuring views with a common container type.
+ * An abstract base class for configuring and managing a view container that connects
+ * to a data model and handles view-related operations.
  *
- * @param CC The type of the common container, which must extend ICommonContainer.
- * @param T The type of the items managed by the common container, which must extend BaseDoc.
- * @param ID The type of the ID field of the items, which must be a non-nullable type.
- * @param V The type of the view data container.
- * @param FILT The type of the API filter used for querying, must extend IApiFilter.
- * @property commonContainer An instance of the common container.
- * @constructor Initializes the ConfigViewContainer with the specified parameters.
+ * This class serves as a specialized configuration container that combines the functionalities
+ * of `ConfigView` and a generalized data container, providing means to handle data views
+ * based on specific configurations, models, and filters.
+ *
+ * @param CC The type of the common container used for managing the data model, must extend `ICommonContainer`.
+ * @param T The type of the document or data entity managed by the container, must extend `BaseDoc`.
+ * @param ID The type of ID used to uniquely identify entities, required to be a non-nullable type.
+ * @param V The type of the view container associated with this configuration, must extend `ViewDataContainer`.
+ * @param FILT The type of the filter applied to the API queries and actions, must extend `IApiFilter`.
+ * @param configData The configuration data used to initialize the container, contains metadata or settings.
+ * @param viewKClass The Kotlin class reference for the view container type.
+ * @param baseUrl Optional base URL used for defining API paths or external resource references.
  */
 abstract class ConfigViewContainer<CC : ICommonContainer<T, ID, FILT>, T : BaseDoc<ID>, ID : Any, V : ViewDataContainer<CC, T, ID, FILT>, FILT : IApiFilter<*>>(
     configData: ConfigData<CC, FILT>,
