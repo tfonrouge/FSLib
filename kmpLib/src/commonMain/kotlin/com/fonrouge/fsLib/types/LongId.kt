3cdfa6abbc5ff33b4c405ae04a0e4002159f559f
Teo Fonrouge <teo@fonrouge.com>
2024-12-17 11:11:09 -0600

    Add documentation for identifier wrapper classes
    Added detailed KDoc comments for OId, LongId, StringId, and IntId classes to describe their purpose, usage, and associated serialization capabilities. This improves code readability and provides clarity on their generic type parameters and properties. No functional changes were introduced.

--- kmpLib/src/commonMain/kotlin/com/fonrouge/fsLib/types/LongId.kt
+++ kmpLib/src/commonMain/kotlin/com/fonrouge/fsLib/types/LongId.kt
@@ -3,6 +3,16 @@ package com.fonrouge.fsLib.types
 import com.fonrouge.fsLib.serializers.LongIdSerializer
 import kotlinx.serialization.Serializable
 
+/**
+ * Represents a generic wrapper for a long identifier.
+ *
+ * This data class encapsulates a long value to be used as an identifier for an associated type `T`.
+ * It provides serialization support through `LongIdSerializer` and overrides the `toString()`
+ * method to return the string representation of the long identifier.
+ *
+ * @param T The type associated with the long identifier. This is currently unused.
+ * @property id The long value of the identifier.
+ */
 @Serializable(with = LongIdSerializer::class)
 data class LongId<@Suppress("unused") T : Any>(
     val id: Long,
