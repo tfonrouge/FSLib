2fd22a9a197d107f0a22efc3bc46609aeeabd5b0
Teo Fonrouge <teo@fonrouge.com>
2025-01-14 15:11:35 -0600

    Refactor waitForLockList to improve flexibility and return type
    Updated the function to accept a generic return type for the `onLock` suspend function, changing the return value to `R?`. Simplified flow collection logic and replaced unnecessary coroutineScope usage with a more streamlined approach.

--- kmpLib/src/commonMain/kotlin/com/fonrouge/fsLib/coroutines/waitForLockList.kt
+++ kmpLib/src/commonMain/kotlin/com/fonrouge/fsLib/coroutines/waitForLockList.kt
@@ -1,29 +1,30 @@
 package com.fonrouge.fsLib.coroutines
 
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flow
 
 /**
- * Attempts to acquire a lock by adding a specified value to a collection, retrying if necessary until the lock is acquired or the attempt count is exhausted.
+ * Attempts to acquire a lock by adding a specified value to a collection, retrying if necessary until the lock
+ * is acquired or the number of attempts is exhausted. If the lock is acquired, the provided suspend function
+ * is executed.
  *
- * @param lockList The mutable collection representing the current locks.
- * @param lockValue The value to be added to the lockList to acquire the lock.
- * @param attempts The number of attempts to acquire the lock before giving up. Defaults to 10.
- * @param delay The delay in milliseconds between attempts to acquire the lock. Defaults to 100.
- * @param onLock An optional suspend function to execute once the lock is acquired.
- * @return A Boolean indicating whether the lock was successfully acquired. Returns `true` if the lock was acquired, otherwise `false`.
+ * @param lockList The mutable collection of locks representing current locked items.
+ * @param lockValue The value to be added to the lockList to attempt acquiring the lock.
+ * @param attempts The number of attempts to acquire the lock before timing out. Defaults to 10.
+ * @param delay The duration in milliseconds to wait between each attempt. Defaults to 100.
+ * @param onLock A suspend function to execute once the lock is successfully acquired.
+ * @return The result of the suspend function executed after acquiring the lock, or `null` if the lock could not be acquired.
  */
 @Suppress("unused")
-suspend fun <T> waitForLockList(
+suspend fun <T, R> waitForLockList(
     lockList: MutableCollection<T>,
     lockValue: T,
     attempts: Int = 10,
     delay: Int = 100,
-    onLock: (suspend () -> Unit)? = null,
-): Boolean {
-    var result = false
-    flow<Boolean> {
+    onLock: (suspend () -> R),
+): R? {
+    val lock = flow<Boolean> {
         var counter = attempts
         while (lockList.contains(lockValue) && counter > 0) {
             --counter
@@ -38,14 +39,11 @@ suspend fun <T> waitForLockList(
             lockList.add(lockValue)
             emit(true)
         }
-    }.collect {
-        result = it
-        onLock?.let {
-            coroutineScope {
-                it()
-            }
+    }.firstOrNull()
+    val result = if (lock == true) {
+        onLock().also {
             lockList.remove(lockValue)
         }
-    }
+    } else null
     return result
 }
