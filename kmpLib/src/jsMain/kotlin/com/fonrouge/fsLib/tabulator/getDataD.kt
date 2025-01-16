549784bc84b0017a60c7603b9db3cf830483a2ac
Teo Fonrouge <teo@fonrouge.com>
2025-01-16 11:58:46 -0600

    Refactor function to support generic return type.
    Updated `getDataD` to allow specifying a generic return type [R], enhancing type safety and flexibility. Adjusted documentation to reflect these changes and ensure clarity.

--- kmpLib/src/jsMain/kotlin/com/fonrouge/fsLib/tabulator/getDataD.kt
+++ kmpLib/src/jsMain/kotlin/com/fonrouge/fsLib/tabulator/getDataD.kt
@@ -4,18 +4,17 @@ import io.kvision.tabulator.js.Tabulator
 import kotlin.reflect.KProperty1
 
 /**
- * Retrieves a nested property value from the dynamic data object associated with the `CellComponent`.
+ * Retrieves nested data from the current cell's data object based on the provided sequence of Kotlin property references.
  *
- * @param path A variable number of `KProperty1` objects that specify the path of properties to traverse
- *             in the data object. Each property represents a step in the path.
- * @return The value of the nested property specified by the given path, or `null` if any step in the
- *         path does not exist in the data object.
+ * @param path A vararg of Kotlin property references representing the path to the desired data.
+ * @return The value at the specified path within the cell's data, cast to the specified generic type [R],
+ *         or null if the data does not exist or cannot be cast to [R].
  */
 @Suppress("unused")
-fun Tabulator.CellComponent.getDataD(vararg path: KProperty1<*, *>): dynamic {
+fun <R> Tabulator.CellComponent.getDataD(vararg path: KProperty1<*, *>): R? {
     var d = getData().asDynamic()
     path.forEach { k ->
         d = d[k.name]
     }
-    return d
+    return d as? R
 }
