f59bcfb392f5ad9dd7bf2ac6238b8d5fd87e5a3e
Teo Fonrouge <teo@fonrouge.com>
2025-04-29 15:16:41 -0600

    Update columnDefinitionDateTime to support nullable OffsetDateTime
    Modified the function to allow properties of type `OffsetDateTime?` instead of just `OffsetDateTime`. This change improves flexibility when creating column definitions for nullable datetime fields. Added a KDoc comment to clarify the purpose and usage of the function.

--- fsLib/src/jsMain/kotlin/com/fonrouge/fsLib/columnDefinition/columnDefinitionDateTime.kt
+++ fsLib/src/jsMain/kotlin/com/fonrouge/fsLib/columnDefinition/columnDefinitionDateTime.kt
@@ -11,9 +11,18 @@ import io.kvision.types.OffsetDateTime
 import kotlin.js.json
 import kotlin.reflect.KProperty1
 
+/**
+ * Creates a column configuration for a Tabulator table, specifically for properties of type `OffsetDateTime?`.
+ *
+ * @param kProperty1 The Kotlin property (`KProperty1`) representing the field in the data model to be used
+ *                   within the column definition.
+ * @param title The title of the column. Defaults to the name of the property if not specified.
+ * @param cellEdited An optional lambda function to handle the event when a cell within this column has been edited.
+ * @return A `ColumnDefinition` object configured with the provided property, title, editor, and formatter.
+ */
 @Suppress("unused")
 fun <T : BaseDoc<*>> columnDefinitionDateTime(
-    kProperty1: KProperty1<T, OffsetDateTime>,
+    kProperty1: KProperty1<T, OffsetDateTime?>,
     title: String = kProperty1.name,
     cellEdited: ((Tabulator.CellComponent) -> Unit)? = null
 ): ColumnDefinition<T> = ColumnDefinition(
