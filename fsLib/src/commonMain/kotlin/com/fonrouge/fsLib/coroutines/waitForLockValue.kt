98061cbb6c26e56bfd190b0ef6ddb5c33f9802c8
Teo Fonrouge <teo@fonrouge.com>
2025-05-15 21:19:21 -0600

    Refactor waitForLockValue to use Mutex for synchronization
    Replaced flow-based locking logic with kotlinx.coroutines Mutex to ensure thread safety and simplify the implementation. General Mutex introduced as a default parameter, improving flexibility and code clarity. Enhanced documentation to reflect the changes and updated locking behavior.

--- fsLib/src/commonMain/kotlin/com/fonrouge/fsLib/coroutines/waitForLockValue.kt
+++ fsLib/src/commonMain/kotlin/com/fonrouge/fsLib/coroutines/waitForLockValue.kt
@@ -1,32 +1,37 @@
 package com.fonrouge.fsLib.coroutines
 
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlin.reflect.KMutableProperty0
 
+private val generalMutex = Mutex()
+
 /**
- * Attempts to acquire a lock by setting a specified `KMutableProperty0` to `true`, retrying if necessary
- * until the lock is acquired or the number of attempts is exhausted. If the lock is acquired, the provided
- * suspend function is executed.
+ * Attempts to acquire a lock by monitoring a mutable Boolean property representing the lock state.
+ * Retries are performed until the lock is available or the maximum number of attempts is reached.
+ * If the lock is successfully acquired, the provided suspend function is executed, and the lock
+ * is released after execution.
  *
- * @param kProperty A mutable property representing the lock state. The lock is considered acquired if
- * the property is successfully set to `true`.
- * @param attempts The number of attempts to acquire the lock before timing out. Defaults to 10.
+ * @param mutex The mutex used to synchronize access to the lock state. Defaults to a general mutex.
+ * @param kProperty A mutable Boolean property representing the lock state. The lock is considered acquired
+ * if the property is set to `false` and then set to `true` during execution.
+ * @param attempts The maximum number of attempts to acquire the lock before giving up. Defaults to 10.
  * @param delay The duration in milliseconds to wait between each attempt. Defaults to 100.
  * @param onLock A suspend function to execute once the lock is successfully acquired.
- * @return The result of the suspend function executed after acquiring the lock, or `null` if the lock could
- * not be acquired.
+ * @return The result of the provided suspend function if the lock was successfully acquired, or null if
+ * the lock could not be acquired within the maximum number of attempts.
  */
 @Suppress("unused")
 suspend fun <R> waitForLockValue(
+    mutex: Mutex = generalMutex,
     kProperty: KMutableProperty0<Boolean>,
     attempts: Int = 10,
     delay: Int = 100,
     onLock: (suspend () -> R),
 ): R? {
-    val locked: Boolean = flow<Boolean> {
-        var counter = attempts
+    var counter = attempts
+    return mutex.withLock {
         while (kProperty.get() && counter > 0) {
             --counter
             if (attempts > 0) {
@@ -34,12 +39,13 @@ suspend fun <R> waitForLockValue(
                 delay(delay.toLong())
             }
         }
-        if (counter == 0) {
-            emit(false)
-        } else {
+        if (counter > 0) {
             kProperty.set(true)
-            emit(true)
+            val r = onLock()
+            kProperty.set(false)
+            r
+        } else {
+            null
         }
-    }.first()
-    return if (locked) onLock().also { kProperty.set(false) } else null
+    }
 }
