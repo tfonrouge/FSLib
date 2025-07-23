e155efa741b7f49817d2149086a1e745d938f3f8
Teo Fonrouge <teo@fonrouge.com>
2025-07-22 21:17:17 -0600

    Add `ifIsNotNullOrEmpty` extension function for nullable collections to `fsLib`.

---
+++ fsLib/src/commonMain/kotlin/com/fonrouge/fsLib/lib/ifIsNotNullOrEmpty.kt
@@ -0,0 +1,11 @@
+package com.fonrouge.fsLib.lib
+
+/**
+ * Executes the given block if the collection is not null and not empty.
+ *
+ * @param block The lambda to execute if the collection is not null and not empty. It receives the non-null and non-empty collection as a parameter.
+ */
+@Suppress("unused")
+fun <T : Collection<E>, E : Any> (T?).ifIsNotNullOrEmpty(block: (T) -> Unit) {
+    if (this != null && this.isNotEmpty()) block(this)
+}
