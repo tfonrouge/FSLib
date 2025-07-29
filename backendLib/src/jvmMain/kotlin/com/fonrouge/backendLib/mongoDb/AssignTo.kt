d0e37a9470c09d01ab19f7756130ecaf4defda8b
Teo Fonrouge <teo@fonrouge.com>
2025-07-29 09:07:05 -0600

    Adjust variance in `AssignTo` to support `in` projection for `KProperty1` definitions

--- backendLib/src/jvmMain/kotlin/com/fonrouge/backendLib/mongoDb/AssignTo.kt
+++ backendLib/src/jvmMain/kotlin/com/fonrouge/backendLib/mongoDb/AssignTo.kt
@@ -4,9 +4,9 @@ import com.fonrouge.backendLib.internal.OnlyInputTypes
 import kotlin.reflect.KProperty1
 
 data class AssignTo<T, @OnlyInputTypes V>(
-    val kField: KProperty1<T, V?>,
+    val kField: KProperty1<in T, V?>,
     val value: V?,
 )
 
 @Suppress("unused")
-infix fun <T, @OnlyInputTypes V> KProperty1<T, V>.assignTo(value: V?) = AssignTo(this, value)
+infix fun <T, @OnlyInputTypes V> KProperty1<in T, V?>.assignTo(value: V?) = AssignTo(this, value)
