1b87d9f59deaeae49f035625b82c46f402bbf4ca
Teo Fonrouge <teo@fonrouge.com>
2025-10-23 18:02:51 -0600

    Add `labelUrl` property: Enhance label-URL pairing logic in `ConfigViewList` for improved accessibility and functionality.

--- fullStack/src/jsMain/kotlin/com/fonrouge/fullStack/config/ConfigViewList.kt
+++ fullStack/src/jsMain/kotlin/com/fonrouge/fullStack/config/ConfigViewList.kt
@@ -71,6 +71,7 @@ abstract class ConfigViewList<CC : ICommonContainer<T, ID, FILT>, T : BaseDoc<ID
         }
 
     override val label: String get() = commonContainer.labelList
+    override val labelUrl: Pair<String, String> by lazy { commonContainer.labelList to url }
 
     fun viewListUrl(apiFilter: FILT = commonContainer.apiFilterInstance()): String =
         urlWithParams(apiFilterParam(apiFilter))
