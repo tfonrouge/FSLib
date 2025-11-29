43377a14ad82b6e9972ffbf7e94e379378cbe6fa
Teo Fonrouge <teo@fonrouge.com>
2025-11-29 11:45:51 -0600

    Refactor `getDataValue`: Improve KDoc, adjust parameters for flexibility, and add an extension for `Object`.

--- fullStack/src/jsMain/kotlin/com/fonrouge/fullStack/tabulator/getDataValue.kt
+++ fullStack/src/jsMain/kotlin/com/fonrouge/fullStack/tabulator/getDataValue.kt
@@ -5,13 +5,14 @@ import js.objects.Object
 import kotlin.reflect.KProperty1
 
 /**
- * Retrieves a value from the data object associated with the cell, following a specified path of properties.
+ * Retrieves a value from a nested object structure based on the provided path of properties.
  *
- * @param path A variable-length list of property references indicating the path to the desired value within the data object.
- * @return The value located at the end of the specified path, or null if the path does not exist or the value is undefined.
+ * @param R The expected return type of the value.
+ * @param path A vararg of property references representing the path to traverse within the object.
+ * @return The value of type R if the path exists and is accessible; otherwise, null.
  */
-fun <R> Tabulator.CellComponent.getDataValue(vararg path: KProperty1<*, *>): R? {
-    var d: Object = getData() as Object
+fun <R> Object.getDataValue(vararg path: KProperty1<*, *>): R? {
+    var d: Object = this
     path.forEach { k ->
         if (!d.hasOwnProperty(k.name)) return null.asDynamic() as R?
         val x = d.asDynamic()[k.name]
@@ -22,6 +23,16 @@ fun <R> Tabulator.CellComponent.getDataValue(vararg path: KProperty1<*, *>): R? 
     }
     return (d.asDynamic()) as R
 }
+
+/**
+ * Retrieves a data value from the CellComponent's data object based on the specified property path.
+ *
+ * @param R The type of the value to retrieve.
+ * @param path A vararg of KProperty1 representing the path to navigate within the data object to extract the value.
+ * @return The value of type R if the path exists and is accessible; otherwise, returns null.
+ */
+fun <R> Tabulator.CellComponent.getDataValue(vararg path: KProperty1<*, *>): R? =
+    (getData() as Object).getDataValue(*path)
 
 /**
  * Retrieves the data value associated with the property name from the given Tabulator cell.
