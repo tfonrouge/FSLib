aa50667cff1fb1335fabdb2463c3feadb0478e06
Teo Fonrouge <teo@fonrouge.com>
2025-12-13 09:38:09 -0600

    Enhance `TabulatorMenuItem` KDoc: Add detailed descriptions for properties and constructor for improved clarity

--- fullStack/src/jsMain/kotlin/com/fonrouge/fullStack/tabulator/TabulatorMenuItem.kt
+++ fullStack/src/jsMain/kotlin/com/fonrouge/fullStack/tabulator/TabulatorMenuItem.kt
@@ -4,19 +4,20 @@ import kotlinx.browser.window
 import org.w3c.dom.events.Event
 
 /**
- * Represents an item in a menu that can be used with a Tabulator table.
+ * Represents a menu item that can be used within a Tabulator-based UI system. A `TabulatorMenuItem`
+ * can contain a label, an optional icon, and other configurable properties such as submenus,
+ * action behavior, and visual features like separators and headers.
  *
- * @param label The display text for the menu item. This text will be wrapped with additional formatting if the
- * menu item is not a header.
- * @param icon The name of the icon class to be displayed alongside the label. If null, no icon will be shown.
- * @param disabled Indicates whether the menu item is disabled. If true, the item will not be clickable.
- * @param separator If true, the menu item will be rendered as a separator.
- * @param menu A nested menu associated with this menu item, allowing for hierarchical menus.
- * @param header If true, the menu item will be treated as a header, and it will not apply additional label formatting.
- * @param url An optional URL to associate with the menu item. This can be used for navigation purposes.
- * @param action A function that will be executed when the menu item is clicked. It takes two parameters:
- * - e: The event that triggered the action.
- * - c: A context object that may be dynamically provided.
+ * @constructor Initializes a TabulatorMenuItem with the provided properties.
+ *
+ * @param label The textual label for the menu item. Defaults to null.
+ * @param icon An optional icon represented as a string, which is typically a CSS class name. Defaults to null.
+ * @param disabled An optional property that indicates whether the menu item is disabled. Defaults to null.
+ * @param separator An optional property to indicate whether the menu item acts as a visual separator. Defaults to null.
+ * @param menu An optional array of sub-menu items (`TabulatorMenuItem`). Defaults to null.
+ * @param header A boolean specifying whether the menu item is used as a header. Defaults to false.
+ * @param url An optional URL associated with the menu item. Defaults to null.
+ * @param action A lambda function defining the action performed when the menu item is interacted with. It takes an event and a dynamic context as arguments.
  */
 @OptIn(ExperimentalJsExport::class)
 @JsExport
