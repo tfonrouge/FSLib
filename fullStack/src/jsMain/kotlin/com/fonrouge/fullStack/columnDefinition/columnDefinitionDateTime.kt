e9ac05326398c9c5a1341c6800ce39629d58a258
Teo Fonrouge <teo@fonrouge.com>
2026-02-23 20:49:46 -0600

    Add `columnDefinitionDateTime` for `Instant` type with custom formatting and alignment

--- fullStack/src/jsMain/kotlin/com/fonrouge/fullStack/columnDefinition/columnDefinitionDateTime.kt
+++ fullStack/src/jsMain/kotlin/com/fonrouge/fullStack/columnDefinition/columnDefinitionDateTime.kt
@@ -8,6 +8,7 @@ import io.kvision.tabulator.Editor
 import io.kvision.tabulator.Formatter
 import io.kvision.tabulator.js.Tabulator
 import io.kvision.types.OffsetDateTime
+import kotlinx.datetime.Instant
 import kotlin.js.json
 import kotlin.reflect.KProperty1
 
@@ -23,6 +24,27 @@ import kotlin.reflect.KProperty1
 @Suppress("unused")
 fun <T : BaseDoc<*>> columnDefinitionDateTime(
     kProperty1: KProperty1<T, OffsetDateTime?>,
+    title: String = kProperty1.name,
+    cellEdited: ((Tabulator.CellComponent) -> Unit)? = null,
+): ColumnDefinition<T> = ColumnDefinition(
+    title = "<i class=\"fa-solid fa-calendar\"></i> $title",
+    headerHozAlign = Align.CENTER,
+    field = fieldName(kProperty1),
+    formatter = Formatter.DATETIME,
+    formatterParams = json(
+        "inputFormat" to "iso",
+        "outputFormat" to "EEE dd MMM y HH:mm",
+        "invalidPlaceholder" to "(invalid date)",
+    ),
+    editor = cellEdited?.let { Editor.DATETIME },
+    editorParams = json(
+        "format" to "iso"
+    ),
+    cellEdited = cellEdited
+)
+
+fun <T : BaseDoc<*>> columnDefinitionDateTime(
+    kProperty1: KProperty1<T, Instant?>,
     title: String = kProperty1.name,
     cellEdited: ((Tabulator.CellComponent) -> Unit)? = null,
 ): ColumnDefinition<T> = ColumnDefinition(
